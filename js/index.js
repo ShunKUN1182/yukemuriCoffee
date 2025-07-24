2cd736d440df1a73ea743818bccc0f7e63e886a0
ShunKUN1182 <2250378@ecc.ac.jp>
2025-07-25 07:41:34 +0900

    試行錯誤

--- js/index.js
+++ js/index.js
@@ -3,9 +3,14 @@ console.log("foo");
 const toggle = document.querySelector("input");
 const lightDark = document.querySelector("body");
 const onOff = document.querySelectorAll("header>h1>img");
+const targetA = document.querySelector(".onsenContent");
+const targetB = document.querySelector(".coffeeContent");
+const targetC = document.querySelector("news");
+const targetD = document.querySelector("map");
 
 
-console.log(onOff);
+
+console.log(targetA);
 
 
 toggle.addEventListener("change" , function(){
@@ -13,13 +18,33 @@ toggle.addEventListener("change" , function(){
     if (lightDark.classList.contains("light")) {
         lightDark.classList.add("dark");
         lightDark.classList.remove("light");
-        // onOff.innerHTML = `"images/OnsenLogo_02.png"`
 
     }else{
         lightDark.classList.add("light");
         lightDark.classList.remove("dark");
-        // onOff.innerHTML = `"images/logo_onsen.png"`
     }
 
 });    
+
+// addEventListener("scroll", function(){
+
+//     for(let i = 0; i = 4; i++){
+        
+//         const getElemantA = targetA.getBoundingClienRect().top;
+//         const getElemantB = targetB.getBoundingClienRect().top;
+//         const getElemantC = targetC.getBoundingClienRect().top;
+//         const getElemantD = targetD.getBoundingClienRect().top;
+
+//         }
+//     })
+    
+
+//     if ( >= 500) {
+//         targetA.classList.add("anm d1");
+//     }else if (getElemant >= 950) {
+//         targetB.classList.add("anm d1");
+//     }
+
+// })
+
 
