41cdf5a34b40121a64abfe222826b236b349b3d5
ShunKUN1182 <2250378@ecc.ac.jp>
2025-07-24 19:53:23 +0900

    js動いた

--- js/index.js
+++ js/index.js
@@ -1,16 +1,22 @@
 console.log("foo");
 
 const toggle = document.querySelector("input");
-const onOff = document.getElementsByClassName("onOff");
+const onOff = document.querySelector("body");
 
 
-if (onOff.classList.contains("off")) {
-    onOff.classList.add("on");
-    onOff.classList.remove("off");
-}
+console.log(onOff);
 
-if (ofOff.classList.contains("on")) {
-    onOff.classList.add("off");
-    onOff.classList.remove("on");
-}
+
+toggle.addEventListener("change" , function(){
+
+    if (onOff.classList.contains("off")) {
+        onOff.classList.add("on");
+        onOff.classList.remove("off");
+    }else{
+        onOff.classList.add("off");
+        onOff.classList.remove("on");
+    }
+    
+
+});    
 
