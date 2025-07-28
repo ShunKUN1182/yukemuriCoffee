bb9b99cb5121ab567c2a4fed0a39a908b6eb8154
ShunKUN1182 <2250378@ecc.ac.jp>
2025-07-28 11:31:08 +0900

    observerを使いたい

--- js/index.js
+++ js/index.js
@@ -3,12 +3,8 @@ console.log("foo");
 const toggle = document.querySelector("input");
 const lightDark = document.querySelector("body");
 const onOff = document.querySelectorAll("header>h1>img");
-// const targetA = document.querySelector(".onsenContent");
-// const targetB = document.querySelector(".coffeeContent");
-// const targetC = document.querySelector("news");
-// const targetD = document.querySelector("map");
-const anm = document.querySelectorAll(".anm");
-
+const kemuri = document.querySelector(".kemuri");
+// const observer = document.querySelector(".observer");
 
 
 // console.log(targetA);
@@ -27,15 +23,20 @@ toggle.addEventListener("change" , function(){
 
 });    
 
-// addEventListener("scroll", function(){
 
-//     for(let i = 0; i = anm.length; i++){
-        
-//         const getElemant = anm.getBoundingClienRect().top;
 
-//         if (window.innerHeight > getElemant) {
-//             anm[i].classList.add("animation");
-//         }
+
+
+
+// console.log(anm[i].getBoundingClientRect().top);
+
+// window.addEventListener("scroll", function(){
+//     for(let i = 0; i = anm.length; i++){        
+        // const getElemant = anm.getBoundingClienRect().top;
+
+        // if (window.innerHeight > getElemant) {
+        //     anm[i].classList.add("animation");
+        // }
 
 //     }
 // })    
