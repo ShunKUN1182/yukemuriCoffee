4f70b03e1c8c0ec81ebbb6e7bfb921c11f36e022
ShunKUN1182 <2250378@ecc.ac.jp>
2025-08-01 12:41:37 +0900

    サーバーにあげました

--- js/index.js
+++ js/index.js
@@ -4,10 +4,16 @@ const toggle = document.querySelector("input");
 const lightDark = document.querySelector("body");
 const onOff = document.querySelectorAll("header>h1>img");
 const kemuri = document.querySelector(".kemuri");
+const sliderImg = document.querySelectorAll(".mainVisual>ul>li");
+const maxCount = sliderImg.length;
+const delay = 3000;
+
 // const observer = document.querySelector(".observer");
 
+console.log(sliderImg , maxCount);
 
-// console.log(targetA);
+
+sliderImg[0].style.opacity = 1;
 
 
 toggle.addEventListener("change" , function(){
@@ -22,6 +28,19 @@ toggle.addEventListener("change" , function(){
     }
 
 });    
+
+
+setInterval( () => {
+
+    console.log("foo");
+
+    for(i = 0; i >= maxCount; i++){
+        sliderImg[i].style.opacity = 0;
+        sliderImg[i + 1].style.opacity = 1;
+    }
+
+    
+},delay);
 
 
 
