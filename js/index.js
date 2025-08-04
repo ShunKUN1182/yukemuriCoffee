3f41bfdb25042b8bcc2863e6595216e89a4da942
ShunKUN1182 <2250378@ecc.ac.jp>
2025-08-04 10:14:24 +0900

    メインビジュアルスライダー完成

--- js/index.js
+++ js/index.js
@@ -5,8 +5,9 @@ const lightDark = document.querySelector("body");
 const onOff = document.querySelectorAll("header>h1>img");
 const kemuri = document.querySelector(".kemuri");
 const sliderImg = document.querySelectorAll(".mainVisual>ul>li");
-const maxCount = sliderImg.length;
-const delay = 3000;
+const maxCount = sliderImg.length - 1;
+const delay = 5000;
+let imagesCount = 0;
 
 // const observer = document.querySelector(".observer");
 
@@ -30,16 +31,24 @@ toggle.addEventListener("change" , function(){
 });    
 
 
+
 setInterval( () => {
 
-    // console.log("foo");
+    console.log(imagesCount);
+    
 
-    for(i = 0; i >= maxCount; i++){
+    for(i = 0; i <= maxCount; i++){
         sliderImg[i].style.opacity = 0;
-        sliderImg[i + 1].style.opacity = 1;
     }
 
-    
+    if(imagesCount < maxCount){
+        imagesCount++;
+    }else{
+        imagesCount = 0;
+    }
+
+    sliderImg[imagesCount].style.opacity = 1;
+
 },delay);
 
 
