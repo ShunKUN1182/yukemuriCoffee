f8f7725a3e68c963dcd162ce399a9f8d4629ee2a
ShunKUN1182 <2250378@ecc.ac.jp>
2025-09-01 16:49:56 +0900

    途中

--- js/introduction.js
+++ js/introduction.js
@@ -3,23 +3,8 @@ const lightDark = document.querySelector("body");
 const toggle = document.querySelector("input");
 const themeColor = localStorage.getItem("mode");
 
-// if (hours >= 18 || hours < 4) {
-//     localStorage.setItem("mode" , "dark");
-//     check.checked = true;
-// }else{
-//     console.log("noon");
-//     localStorage.setItem("mode" , "light");
-// }
-
-// lightDark.classList.add(localStorage.getItem("mode"));
 console.log(themeColor);
 
-// if (themeColor === dark) {
-//     lightDark.classList.add("dark");
-//     check.checked = true;
-// }else{
-//     lightDark.classList.add("light");
-// }
 
 
 lightDark.classList.add(themeColor);
--- js/price.js
+++ js/price.js
@@ -1,1 +1,31 @@
 console.log("foo");
+
+const lightDark = document.querySelector("body");
+const toggle = document.querySelector("input");
+const themeColor = localStorage.getItem("mode");
+
+console.log(themeColor);
+
+
+
+lightDark.classList.add(themeColor);
+
+if (lightDark.classList.contains("dark")) {
+    toggle.checked = true;
+}
+
+toggle.addEventListener("change" , function(){
+
+    if (lightDark.classList.contains("light")) {
+        localStorage.setItem("mode" , "dark");
+        lightDark.classList.add("dark");
+        lightDark.classList.remove("light");
+
+    }else{
+        localStorage.setItem("mode" , "light");
+        lightDark.classList.add("light");
+        lightDark.classList.remove("dark");
+    }
+
+
+});    
